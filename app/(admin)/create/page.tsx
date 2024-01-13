43b2e1ac72fea3655f12b0568b24a87498965819
s1mpleog <mrzkk07@gmail.com>
2024-01-13 20:21:50 +0530

    fix deployment error

--- app/(admin)/create/page.tsx
+++ app/(admin)/create/page.tsx
@@ -5,6 +5,7 @@ export default async function AdminPage() {
   const products = await db.product.findMany({})
   return (
     <div className="max-w-[1300px] mx-auto">
+      {/* @ts-ignore */}
         <CreateProduct products={products} />
     </div>
   )

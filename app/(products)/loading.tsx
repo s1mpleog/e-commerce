185f97e42c7769277e75c22099b020f10db2f1e2
s1mpleog <mrzkk07@gmail.com>
2024-01-14 12:07:30 +0530

    changed loading to spinner

--- app/(products)/loading.tsx
+++ app/(products)/loading.tsx
@@ -1,13 +1,8 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
 export default function loading() {
-  return (
-    <div className="flex items-center justify-center h-screen -mt-20 space-x-4">
-    <Skeleton className="h-[400px] w-[300px] rounded-full" />
-    <div className="space-y-2">
-      <Skeleton className="h-[200px] w-[750px]" />
-      <Skeleton className="h-[100px] w-[400px]" />
-    </div>
-  </div>
-  );
-}
+    return (
+      <div className="flex items-center justify-center h-screen -mt-20">
+          <span className="loader"></span>
+      </div>
+    );
+  }
+  
